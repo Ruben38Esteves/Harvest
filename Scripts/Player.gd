9a01c2e43e4f36e9b7f81328162ea1e95d0e6cb5
Ruben Esteves <ruben38esteves@gmail.com>
2024-01-08 22:50:04 +0000

    leaning when moving sideways

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -45,7 +45,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	if Input.is_action_pressed("sprint"):
+	if Input.is_action_pressed("sprint") and !Input.is_action_pressed("back"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
@@ -53,6 +53,14 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var input_side_right = Input.is_action_pressed("right")
+	var input_side_left = Input.is_action_pressed("left")
+	if input_side_right:
+		head.rotation_degrees.z = lerp(head.rotation_degrees.z, -3.5, delta * 5)
+	elif input_side_left:
+		head.rotation_degrees.z = lerp(head.rotation_degrees.z, 3.5, delta * 5)
+	else:
+		head.rotation_degrees.z = lerp(head.rotation_degrees.z, 0.0, delta * 5)
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
