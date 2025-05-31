1d77e38abab31ec0a1714657137462ac533a3c3d
Ruben Esteves <ruben38esteves@gmail.com>
2025-05-31 22:07:19 +0100

    chest now spawns items

--- Scripts/Interactables/chest.gd
+++ Scripts/Interactables/chest.gd
@@ -2,6 +2,10 @@ extends interactible
 
 var cost: int = 0
 @onready var timer = $Timer
+var common_items = []
+
+func _ready() -> void:
+	common_items.push_back(preload("res://Scenes/Interactables/Items/Broccoli.tscn"))
 
 func interact() -> bool:
 	var player_money = global.player.inventory.items["coins"]
@@ -14,4 +18,15 @@ func interact() -> bool:
 
 
 func _on_timer_timeout():
+	var random_item = get_random_item()
+	var instance = random_item.instantiate()
+	instance.global_position = position
+	get_parent().add_child(instance)
+	instance.global_position.y = instance.global_position.y +1
+	instance.get_child(0).apply_impulse(Vector3(0,2,0))
 	queue_free()
+
+func get_random_item():
+	randomize()
+	var choice = common_items[randi() % common_items.size()]
+	return choice
