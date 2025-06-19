46435020680505ab6addd6f4d22b978af4e1b620
Ruben Esteves <ruben38esteves@gmail.com>
2025-06-19 16:00:42 +0100

    added new zombie model

--- Scripts/Enemies/Zombie/zombie.gd
+++ Scripts/Enemies/Zombie/zombie.gd
@@ -18,14 +18,15 @@ signal zombie_killed
 @onready var nav_agent =$NavigationAgent3D
 @onready var anim_tree = $AnimationTree
 @onready var progress_bar = $SubViewport/ProgressBar
-@onready var sprite = $Sprite3D
+#@onready var sprite = $Sprite3D
 @onready var enemy_health_bar = $EnemyHealthBar
 const blood_particles = preload("res://Scenes/Models/blood_particles.tscn")
 
 @onready var status_effects = $StatusEffects
 #new model
-@onready var animation_tree: AnimationTree = $Zombie_Model/AnimationTree
-@onready var zombie_model: Node3D = $Zombie_Model
+@onready var body = $Body
+@onready var animation_player = $Body/AnimationPlayer
+
 
 
 #coins utils
@@ -36,7 +37,7 @@ var state_machine
 @export var attacking: bool = false
 
 func _ready():
-	state_machine = animation_tree.get("parameters/playback")
+	#state_machine = animation_tree.get("parameters/playback")
 	enemy_health_bar.set_max_health(health)
 	enemy_health_bar.set_health(health)
 
@@ -46,20 +47,25 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _process(delta):
 	velocity
-	match state_machine.get_current_node():
-		"Zombie|ZombieRun":
-			nav_agent.set_target_position(global.player.global_transform.origin)
-			var next_nav_point = nav_agent.get_next_path_position()
-			look_at(Vector3(next_nav_point.x,global_position.y,next_nav_point.z))
-			
-			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-		"Zombie|ZombieBite":
-			velocity = Vector3.ZERO
-	
-	animation_tree.set("parameters/conditions/attack", _target_in_range())
-	animation_tree.set("parameters/conditions/walk", !_target_in_range())
-	
-	animation_tree.get("parameters/playback")
+	#match state_machine.get_current_node():
+		#"Zombie|ZombieRun":
+			#nav_agent.set_target_position(global.player.global_transform.origin)
+			#var next_nav_point = nav_agent.get_next_path_position()
+			#look_at(Vector3(next_nav_point.x,global_position.y,next_nav_point.z))
+			#
+			#velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+		#"Zombie|ZombieBite":
+			#velocity = Vector3.ZERO
+	#
+	#animation_tree.set("parameters/conditions/attack", _target_in_range())
+	#animation_tree.set("parameters/conditions/walk", !_target_in_range())
+	#
+	#animation_tree.get("parameters/playback")
+	animation_player.play("Run")
+	nav_agent.set_target_position(global.player.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	look_at(Vector3(next_nav_point.x,global_position.y,next_nav_point.z))
+	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
 	move_and_slide()
 	
 func _target_in_range():
@@ -89,9 +95,9 @@ func hit(dmg, hit_location = position, status: Array = []):
 			self.get_parent().add_child(instance)
 			queue_free()
 	else:
-		sprite.modulate = Color.DARK_RED
+		#sprite.modulate = Color.DARK_RED
 		await get_tree().create_timer(0.1).timeout
-		sprite.modulate = Color.WHITE
+		#sprite.modulate = Color.WHITE
 		
 		for effect in status:
 			status_effects.apply_status(effect)
