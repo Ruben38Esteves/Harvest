064518f2134644256717c22df4cab7b67fe09294
Ruben Esteves <ruben38esteves@gmail.com>
2025-06-28 19:54:25 +0100

    zombie goes back to idle when reaching position

--- Scenes/Enemies/Zombie/walk_state.gd
+++ Scenes/Enemies/Zombie/walk_state.gd
@@ -4,7 +4,7 @@ extends State
 
 func enter(last_state: String) -> void:
 	var nav_map = parent.get_world_3d().navigation_map
-	var random_point = NavigationServer3D.map_get_random_point(nav_map, 1, true)
+	var random_point = NavigationServer3D.map_get_random_point(nav_map, 1, false)
 	parent.next_nav_point = random_point
 	parent.animation_player.play("Walk")
 	parent.speed = parent.WALK_SPEED
@@ -14,3 +14,5 @@ func update(delta: float) -> void:
 		state_machine.change_state("RunState")
 	if parent.global_position.distance_to(global.player.global_position) < 10.0:
 		state_machine.change_state("RunState")
+	if parent.global_position.distance_to(parent.next_nav_point) <= 1.0:
+		state_machine.change_state("IdleState")
